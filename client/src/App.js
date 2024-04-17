9ef0d2828478b824eb3a430b8e7235784cd226c2
vivekgoswami934 <vivek@rhombuz.io>
2024-04-17 09:40:37 +0530

    initial setup ready and ts added to the project

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline text-amber-700">
+      GoogleDocs Frontend Application
+    </h1>
+  );
+}
+
+export default App;
